59a65344d5ec85b5b9c8ec7de9c9a0d83eda174f
tidusx18 <victoriano518@gmail.com>
2018-06-15 13:05:49 -0400

    Update (build file): Prepend userscript headers

--- dist/CanvasAuditBuild.user.js
+++ dist/CanvasAuditBuild.user.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Canvas Util: Audit Course
+// @namespace    http://github.com/tidusx18
+// @version      0.0.2
+// @description  Audits a Canvas course for ADA criteria and generates a report.
+// @author       Daniel Victoriano <victoriano518@gmail.com>
+// @include      /https:\/\/fiu\.instructure\.com/courses/\d{1,8}$/
+// @grant        GM_xmlhttpRequest
+// @connect      *
+// ==/UserScript==
+
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
