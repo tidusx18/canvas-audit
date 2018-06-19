3f56af4cbf9e3249ae38eb800a3a47720ee2f43b
tidusx18 <victoriano518@gmail.com>
2018-06-19 16:31:19 -0400

    Update: Userscript header version

--- src/userscript-headers/audit-userscript-headers.js
+++ src/userscript-headers/audit-userscript-headers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Canvas Util: Audit Course
 // @namespace    http://github.com/tidusx18
-// @version      0.0.1
+// @version      0.0.3
 // @description  Audits a Canvas course for ADA criteria and generates a report.
 // @author       Daniel Victoriano <victoriano518@gmail.com>
 // @include      /https:\/\/fiu\.instructure\.com/courses/\d{1,8}$/
